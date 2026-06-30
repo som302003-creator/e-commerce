c206adbe157af73da05db831c192159b7be48e7a
somnath kar <somnathkar@somnaths-MacBook-Air.local>
2026-06-30 17:53:48 +0530

    footer ui updater

--- components/layouts/Footer.tsx
+++ components/layouts/Footer.tsx
@@ -1,18 +1,53 @@
-'use client'
-import React from 'react'
-import './Footer.css'
+"use client";
+import React from "react";
+import "./Footer.css";
+import { TiSocialFacebook } from "react-icons/ti";
+import { RiInstagramLine } from "react-icons/ri";
+import { footerdata } from "@/data/footerData";
 
-const Footer:React.FC = () => {
+const Footer: React.FC = () => {
   return (
     <>
-    <div className="footer_section">
-<div className="footer_top"></div>
-<div className="footer_bottom"></div>
-    </div>
-    
-    
+      <div className="footer_section">
+        <div className="footer_top">
+          <div className="logo_left">
+            <div className="logo">ShopEase</div>
+            <div className="description">
+              Building the future of convenient online shopping. We provide
+              verified authentic items straight to your doorstep.
+            </div>
+            <div className="Social_media">
+              <div className="Social_link">
+                <TiSocialFacebook />
+              </div>
+              <div className="Social_link">
+                <RiInstagramLine />
+              </div>
+            </div>
+            <div className="Page_links">
+              {footerdata.map((Section, index) => (
+                <div className="link_column" key={index}>
+                  <div className="column_title">{Section.title}</div>
+                  <ul>
+                    {Section.urlData.map((link, i) => (
+                      <li key={i}>
+                        <a href={link.urlLink}>{link.urlTitle}</a>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="footer_bottom">
+          <div className="copyright">
+            @ copyright ShopEase. All rights reserved.
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- data/footerData.ts
+++ data/footerData.ts
@@ -1,4 +1,5 @@
 import { IFooterLink } from "@/interfaces/footer";
+import { title } from "process";
 
 
 export const footerdata: IFooterLink[] = [
@@ -17,6 +18,22 @@ export const footerdata: IFooterLink[] = [
         urlTitle: "careers",
         urlLink: "#",
       },
+    ],
+  },
+  {
+    title: "Support",
+    urlData: [
+      { urlTitle: "Help Center", urlLink: "#" },
+      { urlTitle: "Refund Policies", urlLink: "#" },
+      { urlTitle: "Submit Ticket", urlLink: "#" },
+    ],
+  },
+  {
+    title: "Legal",
+    urlData: [
+      { urlTitle: "Privacy Terms", urlLink: "#" },
+      { urlTitle: "Terms of Service", urlLink: "#" },
+      { urlTitle: "Cookie Settings", urlLink: "#" },
     ],
   },
 ];
